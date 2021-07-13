310c19821bc65620e01fc47bc8288a8431cc58c7
Zachary Keeley <keeleyz3929@yahoo.com>
2021-07-12 23:57:56 -0600

    Assignment 0

---
+++ script.sh
@@ -0,0 +1,7 @@
+echo "# Computer-Graphics" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/ZacharyKeeley/Computer-Graphics.git
+git push -u origin main
